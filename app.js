5ceb5c530dd070fa32ed9adc6bdfcc2f7a391cef
unknown <david.wendot@gmail.com>
2020-07-14 16:25:47 +0300

    added the account balance

--- app.js
+++ app.js
@@ -37,6 +37,42 @@ app.get('/register', access, (req, res) => {
         }
     )
 }) 
+app.get('/balance', access, (req, res) => {
+    let url = "https://sandbox.safaricom.co.ke/mpesa/accountbalance/v1/query"
+    let auth = "Bearer " + req.access_token 
+
+    request(
+        {
+            url: url, 
+            method: "POST", 
+            headers: {
+                "Authorization": auth
+            }, 
+            json: {
+                "Initiator":"TestInit619",
+                "SecurityCredential":"aFsJJL/FzuxXT9ckTWem4++IJW6OljOQTfOKX/c7uxJZKMD6lOAVtk5B+nMRpbxfbIuAPPwmETl1hHaj1qLLBEh0Uw3cVayYuR6OX9VVjZnE+Dpsclycor8luOzSnAtx8iyNi34OUW4G2pXyLgiSCOTd0C0+dab+kWtnnEFsXp5CJpGNHQCJHCU2WfLQqDQdSWJTh3t8fFQpBuZn41AJru86sm79skQk3l1usjWNrHvfcSgwIgbMeQjZfvbxlBQAF2V2TZarEcTmbLD+IIyJayV0Wf+7UpGqBgNgbVxnwxsN4dWQu7q8cmU7D3MPSTYlmrwDwUVxTHbydZqmdhKEfw==",
+                "CommandID":"AccountBalance",
+                "PartyA":"600618",
+                "IdentifierType":"4",
+                "Remarks":"Remarks",
+                "QueueTimeOutURL":"https://whispering-brook-52781.herokuapp.com/timeout_url",
+                "ResultURL":"https://whispering-brook-52781.herokuapp.com/result_url"
+            }
+        }, 
+        function(error, response, body){
+            if(error){ console.log(error)} 
+            res.status(200).json(body)
+        }
+    )
+}) 
+app.post('/timeout_url', (req, res) => {
+    console.log('......Timeout URL.......') 
+    console.log(req.body)
+}) 
+app.post('/result_url', (req, res) => {
+    console.log('......Result URL.......') 
+    console.log(req.body)
+}) 
 app.post('/confirmation', (req, res) => {
     console.log('......confirmation.......') 
     console.log(req.body)
