363561b5f1bde946355e017cd062c226e4f2f567
unknown <david.wendot@gmail.com>
2020-08-04 12:50:49 +0300

    modified the middleware

--- middleware/access.js
+++ middleware/access.js
@@ -1,4 +1,4 @@
-function access(req, res, next) { 
+ const access = (req, res, next) => { 
     let url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
     let auth = new Buffer("AbHBls0VVrs6oYaek6f8fwuSPok1mjEH:h2A6xCmGCJnAhl2N").toString('base64')
 
