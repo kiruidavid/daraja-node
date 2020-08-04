d0076c4bdcde89536e19ff8cbaad1a457ad318a3
unknown <david.wendot@gmail.com>
2020-08-04 21:02:20 +0300

    added code refactoring

--- config/myConfig.js
+++ config/myConfig.js
@@ -2,6 +2,6 @@ const CONSUMER_KEY = 'AbHBls0VVrs6oYaek6f8fwuSPok1mjEH'
 const CONSUMER_SECRET = 'h2A6xCmGCJnAhl2N'   
 const SHORTCODE = '600618'
 
-module.exports = {CONSUMER_KEY, CONSUMER_SECRET}
+module.exports = {CONSUMER_KEY, CONSUMER_SECRET, SHORTCODE}
 
 
