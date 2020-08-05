7986ca022d97f247aba17b34f6d52af06e4abb1a
unknown <david.wendot@gmail.com>
2020-08-05 18:47:56 +0300

    trial and error

--- routes/paymentsRoutes.js
+++ routes/paymentsRoutes.js
@@ -143,7 +143,7 @@ router.get('/lnm', access, (req, res) => {
 }) 
 router.post('/callback', (req, res) => {
   console.log('....callback....') 
-  console.log(body)
+  console.log(req.body)
 })
 router.post('/timeout_url', (req, res) => {
   console.log('....timeout....')
